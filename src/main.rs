061fd92d2f33ee4b9f234bc928b78f5fc57e58c9
Hosshii <sao_heath6147.wistre@icloud.com>
2020-11-12 22:37:14 +0900

    STEP1 数字を返す

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use ccrs::{input, input_inner, read_value};
+
+fn main() {
+    input! {
+        u: usize,
+    }
+
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!("  mov rax, {}", u);
+    println!("  ret");
+}
