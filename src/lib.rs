c82363a0d18efd7ea16b31f1b9d0e06405fa2c67
Hosshii <sao_heath6147.wistre@icloud.com>
2020-11-14 20:59:00 +0900

    step2 一桁の加算減算

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,47 @@
 pub mod my_macro;
+
+use std::env;
+
+pub fn cc() {
+    input! {
+        s: String
+    }
+    if s == "-help" {
+        help();
+        return;
+    }
+    header();
+
+    println!("main:");
+    let mut iter = s.as_str().chars();
+    if let Some(x) = iter.next() {
+        println!("    mov rax, {}", x);
+    } else {
+        println!("no input found");
+    }
+
+    while let Some(x) = iter.next() {
+        if x == '+' {
+            println!("    add rax, {}", iter.next().unwrap());
+            continue;
+        }
+
+        if x == '-' {
+            println!("    sub rax, {}", iter.next().unwrap());
+            continue;
+        }
+        println!("unexpected token: {}", x);
+        break;
+    }
+    println!("    ret");
+}
+
+fn header() {
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+}
+
+fn help() {
+    println!("this is help");
+    println!("some error occurs");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
-use rs9cc::{input, input_inner, read_value};
+use rs9cc::cc;
 
 fn main() {
-    input! {
-        u: usize,
-    }
-
-    println!(".intel_syntax noprefix");
-    println!(".globl main");
-    println!("main:");
-    println!("  mov rax, {}", u);
-    println!("  ret");
+    cc()
 }
