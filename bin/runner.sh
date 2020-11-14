ef0d9944aaf8a8c54d8eebaf0576a5e3042e69f7
Hosshii <sao_heath6147.wistre@icloud.com>
2020-11-14 20:59:12 +0900

    testscript

--- bin/runner.sh
+++ bin/runner.sh
@@ -5,6 +5,7 @@ build() {
 
 runner() {
     local target="$1"
+    shift
     local builded="./target/x86_64-unknown-linux-musl/debug/rs9cc"
 
     case $target in
@@ -12,7 +13,10 @@ runner() {
         build
         docker run --rm -v $PWD:/rs9cc -w /rs9cc gcc ./bin/test.sh
         ;;
+    "build") build ;;
+    "execute") docker run --rm -v $PWD:/rs9cc -w /rs9cc gcc $@ ;;
     *) echo "no target found" ;;
+
     esac
 }
 
--- bin/test.sh
+++ bin/test.sh
@@ -17,6 +17,7 @@ assert() {
 }
 
 assert 0 0
-assert 42 42
+assert 4 4
+assert 10 "4+9-3"
 
 echo OK
